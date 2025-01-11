807797d391e3dc32c918834dd757c11e4eb2d195
m.chouquet <m.chouquet@student.isartdigital.com>
2025-01-11 01:30:54 +0100

    reset
    reset du projet car il manquait la solution c#

--- Platformer_BriqueRouge/Assets/Scritps/GameObjects/Player.cs
+++ Platformer_BriqueRouge/Assets/Scritps/GameObjects/Player.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public static Player Instance;
-
-    private void Awake()
-    {
-        if (Instance == null) Instance = this;
-    }
-    public static Player GetInstance()
-    {
-        return Instance;
-    }
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
