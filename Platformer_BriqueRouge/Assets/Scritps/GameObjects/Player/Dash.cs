62e8a1c2ea883fd120b3719225abd7ee57f1f5c0
titFournel <160473067+titFournel@users.noreply.github.com>
2025-01-13 12:03:01 +0100

    Add Dash

---
+++ Platformer_BriqueRouge/Assets/Scritps/GameObjects/Player/Dash.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    [SerializeField] private float DashDistance;
+    [SerializeField] private float DashDuration;
+    private float DashSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
