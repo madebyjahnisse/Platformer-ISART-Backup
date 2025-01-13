7d951cfcdbe4d63f0914e9154258a19be135873d
m.chouquet <m.chouquet@student.isartdigital.com>
2025-01-13 10:51:43 +0100

    dev - cameraFollow
    Camera is now smoothly following the player

---
+++ Platformer_BriqueRouge/Assets/Scritps/Managers/CameraManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    [SerializeField] private float _FollowSpeed = 2f;
+    [SerializeField] private float _MaxDistance = 20f;
+    [SerializeField] private float _YOffSet = 5f;
+    [SerializeField] private float _ZOffSet = -10f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(target!=null) FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 lnewPos = new Vector3(target.position.x, target.position.y+_YOffSet, _ZOffSet);
+        float lFollowRatio =  Mathf.Abs(lnewPos.x-transform.position.x);
+        transform.position = Vector3.Slerp(transform.position, lnewPos, _FollowSpeed*(1+lFollowRatio/_MaxDistance)*Time.deltaTime);
+    }
+}
