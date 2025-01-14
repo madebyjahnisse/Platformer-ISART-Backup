60b14f21e8f0e43b253c8c1535865e4390846b84
RyanJhider <ryanjhider@gmail.com>
2025-01-14 01:57:33 +0100

    First Test of Dash
    En gros y'a rien qui marche bien  new input system  testing / Camera zoom Test / Arrow dash Test / Dash hold Test / slow mo

--- Platformer_BriqueRouge/Assets/Scritps/GameObjects/Player/Player.cs
+++ Platformer_BriqueRouge/Assets/Scritps/GameObjects/Player/Player.cs
@@ -2,57 +2,92 @@ using System;
 using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    //Movements:
+    // Movements
+    [Header("Movements")]
     private Vector3 _AccelerationVector = Vector3.zero;
     private Vector3 _Velocity = Vector3.zero;
 
-    //Horizontal Movements :
+    // Horizontal Movements
+    [Header("Horizontal Movements")]
     [SerializeField] private float _AccelerationSpeed = 10f;
     [SerializeField] private float _DeccelerationSpeed = 8f;
     [SerializeField] private float _MaxSpeedHorizontal = 100f;
     [SerializeField] private float _VelocityMin = 0.5f;
 
-    //Vertical Movements :
+    // Vertical Movements
+    [Header("Vertical Movements")]
     [SerializeField] private float _GravityForce = 10f;
     [SerializeField] private float _MaxSpeedVertical = 100f;
     [SerializeField] private float _JumpForce = 50f;
     private bool _Jump = false;
 
-    //Dash
+    // Dash
+    [Header("Dash Settings")]
     [SerializeField] private float _DashDistance = 6f;
     [SerializeField] private float _DashDuration = 1f;
+    private bool _isDashHeld = false;
+    [SerializeField]  private float _dashHoldTime = 0f; 
+    private float _holdStartTime;
 
     private Vector2 _InitMousePosition = Vector2.zero;
     [SerializeField] private float _DashClampAngle = 45f;
     [SerializeField] private float _OffSetMousePositionToDash = 10f;
     private bool _IsDashing;
     private bool _CanDash;
+    private bool _canDash = true;
 
     private bool _DashInit = false;
     private bool _DashConfirm = false;
 
-    //Ground Detection :
-    private bool _IsGrounded = false;
-    private bool _IsHeadColliding = false;
+    //Arrow
+
+    [Header("Arrow Settings")]
+    [SerializeField] private Transform _ArrowPivot;
+    [SerializeField] private float _holdDuration = 0f;
+    [SerializeField] private float _maxScaleY = 0.5f; 
+    [SerializeField] private float _minScaleY = 0f; 
+    [SerializeField] private float _scaleSpeed = 2f;
+
+    //Camera
+    [Header("Camera Settings")]
+    [SerializeField] private Camera _camera; // Cam ref
+    [SerializeField] private float _zoomInFOV = 8f; // FOV dash
+    [SerializeField] private float _zoomOutFOV = 12f; // Default fov
+    [SerializeField] private float _zoomDuration = 0.2f;
+    private float _targetFOV; 
+    private float _originalFOV;
+
+    // Ground Detection
+    [Header("Ground Detection")]
     [SerializeField] private float _RayCastUpDownDistance = 1f;
     [SerializeField] private Vector2 _GroundDetectionBoxSize;
     [SerializeField] private Vector2 _HeadDetectionBoxSize;
     [SerializeField] private LayerMask _GroundLayer;
+    private bool _IsGrounded = false;
+    private bool _IsHeadColliding = false;
 
-    //Side Collisions :
-    private bool _IsRightColliding = false;
-    private bool _IsLeftColliding = false;
+    // Side Collisions
+    [Header("Side Collisions")]
     [SerializeField] private float _RayCastSideDistance = .5f;
     [SerializeField] private Vector2 _SideDetectionBoxSize;
+    private bool _IsRightColliding = false;
+    private bool _IsLeftColliding = false;
 
-    //Tyrolienne :
+    // Tyrolean
+    [Header("Tyrolean System")]
     public bool isTyro = false;
+
+    //Inputs
+
+    public PlayerMovements _Input;
+
 
     #region Singleton
     private static Player _Instance;
@@ -69,16 +104,35 @@ public class Player : MonoBehaviour
             
         }
     }
-
     private void Awake()
     {
         _Instance = this;
+        _Input = new PlayerMovements();
+    }
+    #endregion
+
+    #region Enable/Disable
+
+    private void OnEnable()
+    {
+        _Input.Player.Dash.performed += OnDashHold;
+        _Input.Player.Dash.canceled += OnDashRelease;
+        _Input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _Input.Player.Dash.performed -= OnDashHold;
+        _Input.Player.Dash.canceled -= OnDashRelease;
+        _Input.Disable();
     }
     #endregion
 
     void Start()
     {
-        
+        _camera = Camera.main;
+        _originalFOV = _camera.orthographicSize; 
+        _targetFOV = _originalFOV;
     }
 
     void Update()
@@ -86,13 +140,14 @@ public class Player : MonoBehaviour
         _IsGrounded = IsGrounded();
         Inputs();
         Gravity();
-        DashInit();
+        //DashInit();
         TopOfHeadCollision();
         Jump();
         Deccelerate();
         Accelerate();
         CollideWithWalls();
         Move();
+        ArrowDir();
     }
 
     //DEBUG A SUPPRIMER AVANT CODEREVIEW --------------------------------------
@@ -110,10 +165,13 @@ public class Player : MonoBehaviour
 
     private void Inputs ()
     {
-        _AccelerationVector = new Vector3(Input.GetAxisRaw("Horizontal"),0,0);
-        _Jump = Input.GetKey(KeyCode.Space);
-        _DashInit = Input.GetMouseButtonDown(0);
-        _DashConfirm = Input.GetMouseButtonUp(0);
+        if (_isDashHeld) return;
+
+        if (!_DashInit)
+        {
+            _AccelerationVector = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized;
+            _Jump = Input.GetKey(KeyCode.Space);
+        }
     }
 
 private bool IsGrounded()
@@ -168,6 +226,7 @@ private bool IsGrounded()
         }
         else
         {
+            if (_IsDashing) return;
             _Velocity += -Vector3.up * _GravityForce * Time.deltaTime; //to fall down
         }
     }
@@ -203,37 +262,135 @@ private bool IsGrounded()
     #endregion
 
     #region Dash
-    private void DashInit()
+
+    private void OnDashHold(InputAction.CallbackContext context)
     {
-        if (_DashInit)
+        if (!_isDashHeld)
         {
+            _isDashHeld = true;
+            _holdStartTime = Time.time;  // TimeCode to Hold
+            Debug.Log("Dash Started");
+
             //Play Slow Motion
-            _InitMousePosition = Input.mousePosition;
-            //Set InitMousePosition
-        }
-        if (_DashConfirm)
-        {
-            //Stop SlowMotion
-            //if InitMousePosition != MousePosition : return Clamp Angle between Mouse Position && Player
-            if (Vector2.Distance(_InitMousePosition, Input.mousePosition)> _OffSetMousePositionToDash)
-            {
-                float lAngleBetweenMouseAndPlayer = (180 / Mathf.PI) * Mathf.Atan2(Input.mousePosition.x - _InitMousePosition.x, Input.mousePosition.y - _InitMousePosition.y);
-                Dash(Mathf.Round(lAngleBetweenMouseAndPlayer / _DashClampAngle) * _DashClampAngle);
-            }
-            //if ijkl key pressed get the vector : return angle vector
-            //if android get angle from drag 
+            Time.timeScale = 0.1f;
+
+            // Zoom in
+            _targetFOV = _zoomInFOV;
+            StartCoroutine(ZoomCamera(_zoomDuration));
         }
     }
+
+    private void OnDashRelease(InputAction.CallbackContext context)
+    {
+        if (_isDashHeld)
+        {
+            float holdDuration = Time.unscaledTime - _holdStartTime;
+
+            if (holdDuration >= _dashHoldTime)
+            {
+                Debug.Log("Dash Released");
+                
+                //Stop SlowMotion
+                Time.timeScale = 1f;
+
+                //if InitMousePosition != MousePosition : return Clamp Angle between Mouse Position && Player
+                float lAngleBetweenMouseAndPlayer = (180 / Mathf.PI) * Mathf.Atan2(Input.mousePosition.x - _InitMousePosition.x, Input.mousePosition.y - _InitMousePosition.y);
+                StartCoroutine(PerformDash());
+                Dash(Mathf.Round(lAngleBetweenMouseAndPlayer));
+
+                // Reset zoom
+                _targetFOV = _originalFOV;
+                StartCoroutine(ZoomCamera(_zoomDuration));
+            }
+            else
+            {
+                Debug.Log("Dash Tapped (not long enough to trigger release action) " + holdDuration);
+            }
+
+            _isDashHeld = false;  // Reset Holding
+        }
+    }
+
+    private System.Collections.IEnumerator PerformDash()
+    {
+        _canDash = false;
+        _IsDashing = true;
+
+        yield return new WaitForSeconds(_DashDuration);
+
+        _IsDashing = false;
+        yield return new WaitForSeconds(_DashDuration);
+        if (_IsGrounded)
+            _canDash = true;
+    }
+
+    private IEnumerator ZoomCamera(float duration)
+    {
+        float timeElapsed = 0f;
+        float initialFOV = _camera.orthographicSize;
+
+        while (timeElapsed < duration)
+        {
+            _camera.orthographicSize = Mathf.Lerp(initialFOV, _targetFOV, timeElapsed / duration);
+            timeElapsed += Time.unscaledTime;
+            yield return null;
+        }
+
+        // Assure-toi que le FOV final est bien celui que l'on veut
+        _camera.orthographicSize = _targetFOV;
+    }
+    private void ArrowDir()
+    {
+        Vector3 direction = _AccelerationVector;  
+        direction.z = 0;  // Ignore the Z for rot
+
+        // if moving changing arrow rotation
+        if (direction.magnitude > 0.1f)  
+        {
+            //Calculate angle for arrow
+            float lAngleBetweenPlayerAndDirection = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            _ArrowPivot.rotation = Quaternion.Euler(new Vector3(0, 0, lAngleBetweenPlayerAndDirection - 90));
+        }
+        else
+        {
+            //keep arrow to default pose if no input
+            _ArrowPivot.rotation = Quaternion.Euler(Vector3.zero);
+        }
+
+        // if holding dash
+        if (_isDashHeld)
+        {
+            
+            _holdDuration += Time.unscaledDeltaTime * _scaleSpeed;
+
+            // Clamp size
+            _holdDuration = Mathf.Clamp(_holdDuration, 0f, 1f);
+
+            // lerp Y scale
+            float scaleY = Mathf.Lerp(_minScaleY, _maxScaleY, _holdDuration);
+
+            // apply scale to ArrowPivot
+            _ArrowPivot.localScale = new Vector3(_ArrowPivot.localScale.x, scaleY, _ArrowPivot.localScale.z);
+        }
+        else
+        {
+            // Reset
+            _holdDuration = 0f;
+            _ArrowPivot.localScale = new Vector3(_ArrowPivot.localScale.x, _minScaleY, _ArrowPivot.localScale.z);
+        }
+    }
+
     private void Dash(float pDashAngle)
     {
         Vector3 dashDirection = new Vector3(
             Mathf.Sin(pDashAngle * Mathf.Deg2Rad),
             Mathf.Cos(pDashAngle * Mathf.Deg2Rad), 0
         ).normalized;
-        _Velocity = dashDirection * _DashDistance;
+        Debug.Log(_Velocity + "  B ");
+        _Velocity = _AccelerationVector.normalized * _DashDistance;
+        Debug.Log(_Velocity);
         Debug.Log(dashDirection);
     }
-
 
     #endregion
     private void Move()
@@ -242,4 +399,32 @@ private bool IsGrounded()
     }
 
 
+    // A jeté ?
+    #region Poubelle
+    /*
+private void DashInit()
+{
+    if (_DashInit)
+    {
+        //Play Slow Motion
+        Time.timeScale = 0.1f;
+        _InitMousePosition = Input.mousePosition;
+        //Set InitMousePosition
+    }
+    if (_DashConfirm)
+    {
+        //Stop SlowMotion
+        Time.timeScale = 1f;
+        //if InitMousePosition != MousePosition : return Clamp Angle between Mouse Position && Player
+        if (Vector2.Distance(_InitMousePosition, Input.mousePosition)> _OffSetMousePositionToDash)
+        {
+            float lAngleBetweenMouseAndPlayer = (180 / Mathf.PI) * Mathf.Atan2(Input.mousePosition.x - _InitMousePosition.x, Input.mousePosition.y - _InitMousePosition.y);
+            Dash(Mathf.Round(lAngleBetweenMouseAndPlayer / _DashClampAngle) * _DashClampAngle);
+        }
+        //if ijkl key pressed get the vector : return angle vector
+        //if android get angle from drag 
+    }
+}
+*/
+    #endregion
 }
